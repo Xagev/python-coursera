b30726dc03c90a5593534b84b3d9dd6e71b5fd98
Xagev <52497034+Xagev@users.noreply.github.com>
2019-08-02 04:16:23 +0200

    Create w7_6.py

---
+++ w7_6.py
@@ -0,0 +1,16 @@
+print('\n','BEGIN','\n')
+
+fname=input('Enter the file name: ')
+try:
+    fhand=open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    quit()
+
+count=0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count=count+1
+print('There were ', count, 'subject lines in', fname)
+
+print('\n','END!!!')
